09f0839ff034f61c6a6c6cfcb328b90eb79ccc37
Martin Choutka <martafiixek@gmail.com>
2021-08-12 14:58:21 +0200

    Add page SSR/SSG snippet

---
+++ code-snippets/nextjs/pages/index-ssr.jsx
@@ -0,0 +1,22 @@
+export async function getServerSideProps() {
+  // Volání externí API
+  const res = await fetch("https://.../posts");
+  const data = await res.json();
+
+  // Vrácením props můžeme ve funkci Index získat proměnnou data
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Index({ data }) {
+  return (
+    <div>
+      <h1>Úvodní stránka</h1>
+      <p>Toto je text k uvodní stránce</p>
+      {data}
+    </div>
+  );
+}
---
+++ code-snippets/nextjs/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Úvodní stránka</h1>
+      <p>Toto je text k uvodní stránce</p>
+    </div>
+  );
+}
