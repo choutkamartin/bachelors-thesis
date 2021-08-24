07d5344642a44892e0193b94d2409a26fe62d59a
Martin Choutka <martafiixek@gmail.com>
2021-08-24 12:24:31 +0200

    Add JS snippets

---
+++ code-snippets/js/arrays.js
@@ -0,0 +1,6 @@
+let firstName = ["Petr", "Arnošt", "Jan", "Tomáš"];
+console.log(firstName.length); // Vypíše do konzole počet elementů v poli
+
+firstName.forEach((element) => {
+  console.log(element);
+});
---
+++ code-snippets/js/declaration.js
@@ -0,0 +1,11 @@
+let daysPassed = 32;
+daysPassed = 16;
+let fullName;
+console.log(fullName); // Vypíše do konzole undefined
+
+const daysPassed = 32;
+daysPassed = 16; // Vyhodí chybu
+const fullName = "Martin Choutka";
+
+var daysPassed = 32;
+var fullName = "Martin Choutka";
