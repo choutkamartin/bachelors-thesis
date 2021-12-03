a43209178bcf867c8157b290d80e338b0fc7b78e
Martin Choutka <martafiixek@gmail.com>
2021-12-03 19:12:38 +0100

    Update create-project.js

--- code-snippets/ai/scale/create-project.js
+++ code-snippets/ai/scale/create-project.js
@@ -1,18 +1,20 @@
+// Nastavení HTTP požadavku
 const options = {
     method: 'POST',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        Authorization: 'Basic dsGzdFaB812ULASA41JlOTQwOGFkMzAxN2Y2lakMDo='
+        Authorization: 'Basic dsGzdFaB812ULASA41JlOTQwOGFkMzAxN2Y2lakMDo=' // API klíč
     },
     body: JSON.stringify({
-        type: 'imageannotation',
-        name: 'car_labelling',
-        rapid: false,
-        params: { instruction: 'Please, label the cars.' }
+        type: 'imageannotation', // O jaký typ anotace se má jednat
+        name: 'car_labelling', // Název projektu
+        rapid: false, // Zda se má jednat o Rapid projekt
+        params: { instruction: 'Please, label the cars.' } // Instrukce pro Scale
     })
 };
 
+// Volání API pomocí fetch API
 fetch('https://api.scale.com/v1/projects', options)
     .then(response => response.json())
     .then(response => console.log(response))
